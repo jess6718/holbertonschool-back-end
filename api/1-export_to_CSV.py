85d8896748c82343c07435ea138785304f2eaad5
jess6718 <6718@holbertonstudents.com>
2023-12-18 12:54:23 +0100

    feat: task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Records all tasks that are owned by this employee"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    # return user
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    # return todo filter by userID
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+    # dynamic file name
+    filename = "{}.csv".format(user["id"])
+
+    # close file when written is completed
+    with open(filename, mode="w", newline="") as csv_file:
+        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            write.writerow([user["id"], user["username"], todo["completed"],
+                           todo["title"]])
