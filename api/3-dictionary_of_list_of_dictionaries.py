c901ad779f49c43659320c42121ce9c1a2b47211
jess6718 <6718@holbertonstudents.com>
2023-12-19 03:54:11 +0100

    fix: userId

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,17 +7,18 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    # return user
-    all_user = requests.get(url + "/users").json()
-
+    # return user in json format
+    all_users = requests.get(url + "/users").json()
     todos_all_employees = {}
 
-    for user in all_user:
+    for user in all_users:
         user_id = user["id"]
-        # return all todo of particular employeein json format
-        todos = requests.get(url + "/todos", params={"userid": user_id}).json()
+        # get all todo of particular employeein json format
+        todos = requests.get(url + "/todos", params={"userId": user_id}).json()
+        # store todo data in dictionary format
         user_todos = [{"username": user["username"], "task": todo["title"],
                        "completed": todo["completed"]} for todo in todos]
+        # store all todos of particular user and store in dictionary
         todos_all_employees[user_id] = user_todos
     # close file when written is completed
     with open("todo_all_employees.json", "w") as output:
